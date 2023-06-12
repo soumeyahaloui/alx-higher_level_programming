b7466f1ea7a78e4c46fae4ec24ff42bbe8dd0158
soumeyahaloui <soumeyahaloui@gmail.com>
2023-06-12 15:28:05 +0000

    updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,9 +3,7 @@
 
 def element_at(my_list, idx):
     """Retrieve an element from a list."""
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+    return my_list[idx] if 0 <= idx < len(my_list) else None
 
 # Test the function
 if __name__ == "__main__":
