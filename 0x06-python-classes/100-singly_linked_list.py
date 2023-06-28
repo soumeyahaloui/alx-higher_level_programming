fbd3cd3fe97edcc048708b3c997581bd2bf01bb0
soumeyahaloui <soumeyahaloui@gmail.com>
2023-06-28 02:22:09 +0000

    updated

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,62 @@
+class Node:
+    """Represents a node of a singly linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initializes a node with the given data and next_node."""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrieves the data of the node."""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Sets the data of the node."""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieves the next node."""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Sets the next node."""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Represents a singly linked list."""
+
+    def __init__(self):
+        """Initializes an empty singly linked list."""
+        self.head = None
+
+    def sorted_insert(self, value):
+        """Inserts a new node into the correct sorted position in the list (increasing order)."""
+        new_node = Node(value)
+
+        if self.head is None or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node is not None and value >= current.next_node.data:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """Returns a string representation of the singly linked list."""
+        result = ""
+        current = self.head
+        while current is not None:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result.rstrip()
