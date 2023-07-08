eb07a8672e5da460eecff4446ed0fd5d5615550a
soumeyahaloui <soumeyahaloui@gmail.com>
2023-07-08 01:32:45 +0000

    updated

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,4 +1,5 @@
-dd_integer = __import__('0-add_integer').add_integer
+#!/usr/bin/python3
+add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(1, 2))
 print(add_integer(100, -2))
