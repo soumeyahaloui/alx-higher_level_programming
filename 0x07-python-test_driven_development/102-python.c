91b52581d08fbc326f47fff5ae1a93d3d5cdd939
soumeyahaloui <soumeyahaloui@gmail.com>
2023-07-08 02:41:23 +0000

    updated

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,45 +1,32 @@
-#include <Python.h>
+/*
+ * File: 102-python.c
+ */
 
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
 void print_python_string(PyObject *p)
 {
-    PyUnicodeObject *string = (PyUnicodeObject *)p;
-    Py_ssize_t length;
-    Py_UCS4 *value;
-    Py_UCS4 max_ucs4 = 0;
-    int is_ascii = 1;
-    int i;
+	long int length;
 
-    if (!PyUnicode_Check(string))
-    {
-        fprintf(stderr, "[.] string object info\n");
-        fprintf(stderr, "  [ERROR] Invalid String Object\n");
-        return;
-    }
+	fflush(stdout);
 
-    length = PyUnicode_GET_LENGTH(string);
-    value = PyUnicode_AsUCS4Copy(string);
-    if (!value)
-    {
-        fprintf(stderr, "[.] string object info\n");
-        fprintf(stderr, "  [ERROR] Invalid String Object\n");
-        return;
-    }
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
 
-    for (i = 0; i < length; i++)
-    {
-        if (value[i] >= 128)
-        {
-            is_ascii = 0;
-            break;
-        }
-        if (value[i] > max_ucs4)
-            max_ucs4 = value[i];
-    }
+	length = ((PyASCIIObject *)(p))->length;
 
-    fprintf(stderr, "[.] string object info\n");
-    fprintf(stderr, "  type: %s\n", is_ascii ? "compact ascii" : "compact unicode object");
-    fprintf(stderr, "  length: %ld\n", length);
-    fprintf(stderr, "  value: %ls\n", (wchar_t *)value);
-
-    free(value);
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
