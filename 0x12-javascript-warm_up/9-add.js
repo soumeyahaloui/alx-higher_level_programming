cd9b4f83fbe7d84b99145419a2c7bca7ce775123
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-23 17:15:01 -0700

    updated

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  return a + b;
+  const c = a + b;
+  console.log(c);
 }
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('NaN');
-} else {
-  const result = add(num1, num2);
-  console.log(result);
-}
+add(Number(process.argv[2]), Number(process.argv[3]));
