36406b42f13d939465cb2fd2bb4d460c63660a52
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-28 10:06:23 -0700

    updated

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends an OPTIONS request to a URL and displays the allowed HTTP methods
 
-curl -sI "$1" | grep "Allow" | cut -d' ' -f2-
+curl -sI -X OPTIONS "$1" | grep "Allow" | cut -d' ' -f2-
