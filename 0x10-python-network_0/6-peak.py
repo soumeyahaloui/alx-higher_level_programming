634fc4469d72699d9e57d62a260b75d62c0f2086
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-28 09:57:38 -0700

    updated

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
+
+# Test cases
+if __name__ == "__main__":
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
