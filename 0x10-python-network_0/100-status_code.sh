cb4905ff63adc61fca25f3e89a0c5a0ba7429678
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-28 10:33:03 -0700

    updated

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and displays only the status code of the response
-
-response=$(curl -sI -o /dev/null -w "%{http_code}" "$1")
-echo "$response"
+# sends a request to a URL passed as an argument, and displays only the status code of the response.
+curl -o /dev/null -sw "%{http_code}" $1
