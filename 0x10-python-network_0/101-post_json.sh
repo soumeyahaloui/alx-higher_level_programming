0be0be8e69570c48c62d86dcdeaa86a35adc73ac
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-28 11:10:20 -0700

    updated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL and displays the body of the response
-
-# Check if the second argument (JSON file) exists
-if [ ! -f "$2" ]; then
-    echo "Usage: $0 <URL> <JSON file>"
-    exit 1
-fi
-
-# Check if the JSON content in the file is valid
-if ! jq empty < "$2" > /dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-# Send the POST request and display the response body
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
