e006807db774b4224b439a9c63f34d0fc1643e58
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-28 10:29:50 -0700

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,29 @@
+#!/usr/bin/python3
+
+
 def find_peak(list_of_integers):
-    if not list_of_integers:
+
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
-    left, right = 0, len(list_of_integers) - 1
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
 
-    while left < right:
-        mid = (left + right) // 2
+    mid_idx = int(len(list_of_integers) / 2)
 
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
         else:
-            right = mid
+            return list_of_integers[mid_idx - 1]
 
-    return list_of_integers[left]
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
 
-# Test cases
-if __name__ == "__main__":
-    print(find_peak([1, 2, 4, 6, 3]))
-    print(find_peak([4, 2, 1, 2, 3, 1]))
-    print(find_peak([2, 2, 2]))
-    print(find_peak([]))
-    print(find_peak([-2, -4, 2, 1]))
-    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+    return find_peak(a_list)
