fde1e38f693ec1ee2ee2cd6e62e8d84cadfbbfc6
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-28 09:42:28 -0700

    updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # This script sends a GET request to a URL and displays the body of the response for a 200 status code
-curl -sI "$1" | grep -i "HTTP/1.1 200 OK" && curl -s "$1"
+
+response=$(curl -sI "$1")
+
+if [[ $response == *"HTTP/1.1 200 OK"* ]]; then
+    curl -s "$1"
+fi
