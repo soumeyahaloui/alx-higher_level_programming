69d292cc3e19c98341e26360689cd8d081db1daa
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-29 14:06:21 -0700

    updated

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-Sends a request to a URL and displays the body of the response (decoded in utf-8).
-Handles urllib.error.HTTPError exceptions and prints the HTTP status code.
-"""
+"""Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
 
-import urllib.request
-import urllib.error
-import sys
+from urllib import request, error
+from sys import argv
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
+        try:
+            with request.urlopen(argv[1]) as page:
+                print(page.read().decode('utf-8'))
+        except error.HTTPError as e:
+            print("Error code: {}".format(e.code))
