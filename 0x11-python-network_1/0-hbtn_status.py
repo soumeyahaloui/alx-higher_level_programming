167bd7aa95b99b607515ac42004188d608fca488
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-29 13:30:34 -0700

    updated

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", utf8_content)
