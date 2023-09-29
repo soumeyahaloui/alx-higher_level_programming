51445a5a3b30edee68a97df8e2a3cd95f25b59fa
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-28 18:51:52 -0700

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
-"""
-This module provides a script to list all states from the database hbtn_0e_0_usa.
-"""
-
+"""lists all states from the database"""
 import MySQLdb
-import sys
-
-def list_states(username, password, database):
-    """
-    Lists all states from the specified database.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-    """
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    list_states(username, password, database)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
