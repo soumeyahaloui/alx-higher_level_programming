80a6c770e161cb532859a42e2028d7aa73f0da72
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-29 13:35:25 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-Module that fetches https://intranet.hbtn.io/status
-"""
+"""Python script that fetches https://intranet.hbtn.io/status."""
 
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
+    with request.urlopen("https://intranet.hbtn.io/status") as page:
+        content = page.read()
         print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", content)
-        print("\t- utf8 content:", utf8_content)
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
