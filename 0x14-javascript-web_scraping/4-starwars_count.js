98cfe422dc2c4faec8066df613a54e0fed74c303
soumeyahaloui <soumeyahaloui@gmail.com>
2023-10-24 15:56:52 +0000

    updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = 18; // Wedge Antilles' character ID
-
-request(apiUrl, (error, response, body) => {
+request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
-  } else {
-    const filmsData = JSON.parse(body).results;
-    const count = filmsData.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    console.log(count);
   }
+  /* const nb = JSON.parse(body).results.reduce((acc, elem) => {
+    acc += elem.characters.reduce((acc, character) => {
+      return (character === 'https://swapi.co/api/people/18/' ? acc + 1 : acc);
+    }, 0);
+    return (acc);
+  }, 0);
+  */
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.filter((url) => { return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
 });
