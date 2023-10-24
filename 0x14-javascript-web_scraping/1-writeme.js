9e3bfae217505674ff18f15c0f9f09b2b63909ca
soumeyahaloui <soumeyahaloui@gmail.com>
2023-10-24 15:35:10 +0000

    updated

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,7 +7,5 @@ const content = process.argv[3];
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log('The file has been written successfully!');
   }
 });
